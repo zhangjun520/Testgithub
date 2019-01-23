a4f075110f2f9e0a281cb5b8f5357c4f2134ae52
zhangjun520 <541157099@qq.com>
2019-01-23 17:41:01 +0800

    创建新的控制器

---
+++ 测试test/ViewController2.m
@@ -0,0 +1,32 @@
+//
+//  ViewController2.m
+//  测试test
+//
+//  Created by admin on 2019/1/23.
+//  Copyright © 2019 zhangjun@541157099. All rights reserved.
+//
+
+#import "ViewController2.h"
+
+@interface ViewController2 ()
+
+@end
+
+@implementation ViewController2
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor grayColor];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
